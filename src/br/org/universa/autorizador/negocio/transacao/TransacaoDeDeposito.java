03edca1f1b5a166607304ccf0939158a4e1fa38f
Bruno Oliveira <brunobdantas@gmail.com>
2012-10-02 22:36:13 -0300

    Signed-off-by: Bruno Oliveira <brunobdantas@gmail.com>

--- src/br/org/universa/autorizador/negocio/transacao/TransacaoDeDeposito.java
+++ src/br/org/universa/autorizador/negocio/transacao/TransacaoDeDeposito.java
@@ -1,11 +1,14 @@
 package br.org.universa.autorizador.negocio.transacao;
 
+import br.org.universa.autorizador.negocio.conta.Conta;
+import br.org.universa.autorizador.negocio.conta.ContaMediator;
+
 public class TransacaoDeDeposito extends AbstractTransacaoMediator {
 
 	@Override
 	protected void executaRegrasEspecificas(Transacao transacao) {
-		// TODO Auto-generated method stub
-
+		Conta conta = ContaMediator.get().consultaConta(transacao.getAgencia(),
+				transacao.getConta());
+		ContaMediator.get().atualiza(conta);
 	}
-
 }
